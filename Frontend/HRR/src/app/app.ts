bf30330acac72849c68e9fb757cdae557e9e2e7c
asaleh27-2003 <ahmadhalaweh47@gmail.com>
2026-08-18 18:10:27 +0300

    retreival information on App components and how it works etc...

--- Frontend/HRR/src/app/app.ts
+++ Frontend/HRR/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgClass, NgIf, NgStyle, } from '@angular/common';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf,NgFor,NgClass,NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HRR');
-}
+stu=[
+{name:'stu1',mark:89},
+{name:'stu2',mark:57},
+{name:'stu3',mark:90},
+{name:'stu4',mark:94},
+{name:'stu5',mark:49},
+]}
